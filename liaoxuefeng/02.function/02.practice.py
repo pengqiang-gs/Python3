45a40faa0a173a1f61c1fc959fec1c5ccbbcdc80
pengqiangnj <pengqiang.gs.nj@gmail.com>
2018-04-16 20:50:20 +0800

    廖雪峰Python3学习笔记：Python函数，汉诺塔练习

--- liaoxuefeng/02.function/02.practice.py
+++ liaoxuefeng/02.function/02.practice.py
@@ -1,17 +1,28 @@
 # _*_ coding: utf-8 _*_
 
-def move(N, A, B, C):
-    if isinstance(N, (int, float)):
-        if N == 1:
-            print(A, ' --> ', C)
-        else:
-            move(N-1, A, C, B)
-            move(1, A, B, C)
-            move(N-1, B, A, C)
+counter = 0
+def move(disk_number, piller_1, piller_2):
+	global counter
+	counter += 1
+	print('第%d次移动：把%d号盘子从柱子%s移动到柱子%s' % 
+			(counter, disk_number, piller_1, piller_2))
 
-    else:
-        raise TypeError('invalid type of parameter.')
+def hanoi_tower(N, A, B, C):
+	if isinstance(N, (int, float)):
+		if N == 1:
+			move(1, A, C)
+		else:
+			# move N-1 disks to piller B which based piller A
+			hanoi_tower(N-1, A, C, B)
+			
+			# move left disk to piller C
+			move(N, A, C)
+			
+			# move N-1 disks to piller C which based piller B
+			hanoi_tower(N-1, B, A, C)
+	else:
+		raise TypeError('invalid type of parameter.')
 
 if __name__ == '__main__':
-    number = input('请输入汉诺塔A上的盘子数目：')
-    move(int(number), 'A', 'B', 'C')
+	number = int(input('请输入汉诺塔A上的盘子数目：'))
+	hanoi_tower(number, 'A', 'B', 'C')
